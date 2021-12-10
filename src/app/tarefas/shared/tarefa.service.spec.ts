ff16c72ecaf08df69beb163dabbff3f0b2addd65
berkson <berksonx@yahoo.com.br>
2021-12-10 17:44:14 -0300

    testes do serviço de tarefas conccluido

--- src/app/tarefas/shared/tarefa.service.spec.ts
+++ src/app/tarefas/shared/tarefa.service.spec.ts
@@ -22,4 +22,20 @@ describe('TarefaService', () => {
     let total = service.listartodos().length;
     expect(total).toEqual(2);
   });
+
+  it('deve acrescentar uma tarefa a lista', ()=>{
+    service.cadastrar(new Tarefa(789, 'testes3', false));
+    expect(service.listartodos().length).toEqual(3);
+  });
+
+  it('deve retornar a tarefa de id 456', () =>{
+    let tarefa = service.buscarPorId(456);
+    expect(tarefa?.nome).toEqual('teste2');
+  });
+
+  it('deve alterar o status da tarefa de id 456 para true', ()=>{
+    expect(service.buscarPorId(456)?.concluida).toEqual(false);
+    service.alterarStatus(456);
+    expect(service.buscarPorId(456)?.concluida).toEqual(true);
+  });
 });
