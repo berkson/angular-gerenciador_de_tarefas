0dde45b2445849c2c11ab451269cd71a2572c8b2
berkson <berksonx@yahoo.com.br>
2021-12-10 21:36:01 -0300

    componente de cadastramento concluido

--- src/app/tarefas/index.ts
+++ src/app/tarefas/index.ts
@@ -1,6 +1,5 @@
-import { from } from 'rxjs';
-
 export * from './tarefas.module';
 export * from './shared';
 export * from './listar';
 export * from './tarefas.routing.module';
+export * from './cadastrar';
