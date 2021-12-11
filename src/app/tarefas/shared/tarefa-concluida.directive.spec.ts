0995543c3fa9ca9ca0e692a75d6f92a4904969c6
berkson <berksonx@yahoo.com.br>
2021-12-10 23:55:46 -0300

    correçao do teste de criaçao da diretiva.

--- src/app/tarefas/shared/tarefa-concluida.directive.spec.ts
+++ src/app/tarefas/shared/tarefa-concluida.directive.spec.ts
@@ -1,8 +1,20 @@
+import createSpyObj = jasmine.createSpyObj;
+import { Directive, ElementRef } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
 import { TarefaConcluidaDirective } from './tarefa-concluida.directive';
+
+let element: ElementRef = createSpyObj('idNaicsRef', ['nativeElement']);
+
+beforeEach(async () => {
+  await TestBed.configureTestingModule({
+    declarations: [ TarefaConcluidaDirective ]
+  })
+  .compileComponents();
+});
 
 describe('TarefaConcluidaDirective', () => {
   it('should create an instance', () => {
-    const directive = new TarefaConcluidaDirective();
+    const directive = new TarefaConcluidaDirective(element);
     expect(directive).toBeTruthy();
   });
 });
